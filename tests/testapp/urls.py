653798b76d6112d426dd1fe55ed0bfe3ab64fec3
Panagiotis H.M. Issaris <takis@issaris.com>
2022-11-30 14:37:25 +0100

    Example views demonstrating show_urls decorator option being broken, #1773

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -16,15 +16,42 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path
 from django.contrib import admin
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponse
+from django.urls import path
 
 login_view = auth_views.LoginView.as_view() if hasattr(auth_views, 'LoginView') else auth_views.login
 logout_view = auth_views.LogoutView.as_view() if hasattr(auth_views, 'LogoutView') else auth_views.logout
+
+
+def example_view_no_decorator(request):
+    return HttpResponse()
+
+
+@login_required
+def example_view_decorator_login_required(request):
+    return HttpResponse()
+
+
+@permission_required("loading.view_list")
+def example_view_decorator_permission_required(request):
+    return HttpResponse()
+
+
+@staff_member_required
+def example_view_decorator_staff(request):
+    return HttpResponse()
+
 
 urlpatterns = [
     path('login/', login_view, {'template_name': 'login.html'}, name="login"),
     path('logout/', logout_view, name="logout"),
     path('admin/', admin.site.urls),
+    path("example_no_decorator", example_view_no_decorator),
+    path("example_login_required", example_view_decorator_login_required),
+    path("example_permission_required", example_view_decorator_permission_required),
+    path("example_staff", example_view_decorator_staff),
 ]
